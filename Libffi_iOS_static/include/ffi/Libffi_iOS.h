06cfc673a31cfc1151af081399479e1d777f23af
faimin <fuxianchao@gmail.com>
2020-12-23 20:50:32 +0800

    static library

---
+++ Libffi_iOS_static/include/ffi/Libffi_iOS.h
@@ -0,0 +1,20 @@
+//
+//  Libffi_iOS.h
+//  Libffi-iOS
+//
+//  Created by Zero.D.Saber on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Libffi_iOS.
+FOUNDATION_EXPORT double Libffi_iOSVersionNumber;
+
+//! Project version string for Libffi_iOS.
+FOUNDATION_EXPORT const unsigned char Libffi_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Libffi_iOS/PublicHeader.h>
+
+#import <Libffi_iOS/ffi.h>
+
+
